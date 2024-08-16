4e7f78b35f8d8bfdd8fc3f03486d2bfa0aeb4d98
Carlossxdxd <robertomerc83@gmail.com>
2024-08-15 20:13:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Era obvio, ya sabia')
+    alert('Mandalo por messenger :), Graciass')
 });
 
 const noBtn = document.querySelector('#noBtn');
